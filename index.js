670b9886ca7205c3c45ee5a7a45cd426badc3a71
CodeZhama <hpdev2604@gmail.com>
2022-08-23 17:43:29 +0500

    first-change

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const acc_btns = document.querySelectorAll(".accardion__header");
 const acc_contents = document.querySelectorAll(".accardion__body");
-console.log(acc_btns);
 acc_btns.forEach((btn) => {
   btn.addEventListener("click", (event) => {
     btn.classList.toggle("active");
@@ -15,3 +14,22 @@ acc_btns.forEach((btn) => {
     }
   });
 });
+
+// Coutdown time//
+const daysDiv = document.getElementById("days");
+const minuteSpan = document.getElementById("minute__id");
+const secondySpan = document.getElementById("secondy__id");
+
+let dets = new Date("sep 1, 2022 10:00:00").getTime();
+
+let time = setInterval(function () {
+  let nowTime = new Date().getTime();
+
+  let days = Math.floor((dets - nowTime) / (1000 * 60 * 60 * 24));
+  let minutes = Math.floor(((dets - nowTime) % (1000 * 60 * 60)) / (1000 * 60));
+
+  let secondy = Math.floor(((dets - nowTime) % (1000 * 60)) / 1000);
+  daysDiv.innerText = days;
+  minuteSpan.innerText = minutes;
+  secondySpan.innerText = secondy;
+}, 1000);
